c6d1939358f11541880f9f713590bd73251e2fca
Fariz101 <fariz_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:01:31 +0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,25 @@
+public class hello {
+    public static void main(String[] args) {
+        
+        String nama="Fariz";
+        String asal_Kota="Malang";
+        int usia=14;
+        double beratBadan=43.00;
+        float tingiBadan=160.122143452f;
+        char jenisKelamin = 'L';
+        boolean status = false;
+
+        System.out.println("Nama:"+ nama);
+        System.out.println("Asal Kota:"+asal_Kota);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat Badan:"+beratBadan);
+        System.out.println("Tinggi Badan:"+tingiBadan);
+        System.out.println("Jenis Kelamin:"+jenisKelamin);
+        System.out.println("Status:"+status);
+        
+
+     
+        
+    }
+
+}
