8ecfa811c7ea7ad347c02a08166bdc68888f25eb
Fariz101 <fariz_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:59:53 +0700

    Add files via upload

---
+++ tambah.java
@@ -0,0 +1,8 @@
+public class tambah {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 200;
+        int hasil = a+b;
+        System.out.println("hasil dari "+a+" ditambah "+b+" adalah "+hasil);
+    }
+}
